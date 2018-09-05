dfb612866102bc4390008b91431e82fc7eaad44c
Katia Borysova <katiaborysova06@gmail.com>
2018-09-05 17:47:34 +0300

    updated js files

--- app/js/slider.js
+++ app/js/slider.js
@@ -5,44 +5,51 @@ var animationDelay = 3500;
 
 var slider = document.querySelector('.slider');
 
-slider.addEventListener('click', function(event) {
-    if (!event.target.classList.contains('slider__icon')) return;
+if(slider) {
 
-    currentSlide = parseInt(event.target.getAttribute('data-id'));
-    showSliderId(currentSlide);
-
-    var id = event.target.getAttribute('data-id');
-
-    showSliderId(id);
-});
-
-initSlider();
-
-
-function showSliderId(id) {
-    var current = slider.querySelector('[data-slide="'+ id + '"]');
-
-    slider.querySelectorAll('.slider__slide').forEach( function(slide) {
-        slide.style.opacity = 0;
+    slider.addEventListener('click', function(event) {
+        if (!event.target.classList.contains('slider__icon')) return;
+    
+        currentSlide = parseInt(event.target.getAttribute('data-id'));
+        showSliderId(currentSlide);
+    
+        var id = event.target.getAttribute('data-id');
+    
+        showSliderId(id);
+    });
+    
+    initSlider();
+    
+    
+    function showSliderId(id) {
+        
+        var current = slider.querySelector('[data-slide="'+ id + '"]');
+    
+        slider.querySelectorAll('.slider__slide').forEach( function(slide) {
+            slide.style.opacity = 0;
+            setTimeout(function() {
+                slide.classList.add('hide');
+            },delay);
+        }) ;
+    
         setTimeout(function() {
-            slide.classList.add('hide');
-        },delay);
-    }) ;
+            current.classList.remove('hide');
+            current.style.opacity = 1;
+        },delay)
+    };
+    
+    function initSlider() {
+        setInterval(function() {
+            showSliderId(getSliderId());
+        },animationDelay)
+    };
+    
+    function getSliderId() {
+        currentSlide++;
+        currentSlide = currentSlide % 4 === 0 ? 1 : currentSlide;
+        return currentSlide;
+    };
+    
 
-    setTimeout(function() {
-        current.classList.remove('hide');
-        current.style.opacity = 1;
-    },delay)
 };
-
-function initSlider() {
-    setInterval(function() {
-        showSliderId(getSliderId());
-    },animationDelay)
-};
-
-function getSliderId() {
-    currentSlide++;
-    currentSlide = currentSlide % 4 === 0 ? 1 : currentSlide;
-    return currentSlide;
-};
+ 
